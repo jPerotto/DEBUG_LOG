80923a48ede8957bca75d462562515354f99d319
Perotto <julianoperotto@gmail.com>
2020-07-13 10:18:25 -0300

    Adicionado CREATE_PRINT
    Uma forma de fazer o print sem colocar o pathTOFileName __LINE__ e __FUNCTION__

--- src/DEBUG_LOG.h
+++ src/DEBUG_LOG.h
@@ -38,6 +38,7 @@ static uart_t _uart_bus_array[3] = {
 #endif
 
 #define CREATE_LOG(letter, log)  "[" #letter "][%s:%u] %s(): " log "\r\n", pathToFileName(__FILE__), __LINE__, __FUNCTION__
+#define CREATE_PRINT(letter, log)  "[" #letter "]: " log "\r\n"
 
 //#define LOG_BLE(log, ...) LOG_print(CREATE_LOG(BLE, log), ##__VA_ARGS__)
 //#define LOG_BLE(format, ...)
@@ -76,5 +77,30 @@ int LOG_print(const char *format, ...)
     free(temp);
   }
   return len;
+}
+
+String LOG_string(const char *format, ...)
+{
+  static char loc_buf[64];
+  char * temp = loc_buf;
+  int len;
+  va_list arg;
+  va_list copy;
+  va_start(arg, format);
+  va_copy(copy, arg);
+  len = vsnprintf(NULL, 0, format, arg);
+  va_end(copy);
+  if (len >= sizeof(loc_buf)) {
+    temp = (char*)malloc(len + 1);
+    if (temp == NULL) {
+      return String("NULL LOG");
+    }
+  }
+  vsnprintf(temp, len + 1, format, arg);
+  va_end(arg);
+  if (len >= sizeof(loc_buf)) {
+    free(temp);
+  }
+  return String(temp);
 }
 #endif
