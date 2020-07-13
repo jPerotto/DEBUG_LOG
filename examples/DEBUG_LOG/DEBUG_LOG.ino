8075bc192d425f978ae733141a601f24a4baa0be
Perotto <julianoperotto@gmail.com>
2020-07-13 13:22:30 -0300

    Atualização do example

--- examples/DEBUG_LOG/DEBUG_LOG.ino
+++ examples/DEBUG_LOG/DEBUG_LOG.ino
@@ -1,13 +1,20 @@
 #include <DEBUG_LOG.h>
 
-#define LOG_BLE(log, ...) LOG_print(CREATE_LOG(BLE, log), ##__VA_ARGS__)
-//#define LOG_BLE(format, ...)
+#define LOG_LOG(log, ...) LOG_print(CREATE_LOG(LOG, log), ##__VA_ARGS__)
+#define LOG_STRING_FULL(log, ...) LOG_string(CREATE_LOG(STR, log), ##__VA_ARGS__)
+#define LOG_STRING_LOG(log, ...) LOG_string(CREATE_LOG(STR, log), ##__VA_ARGS__)
+
+//#define LOG_LOG(log, ...) 
+//#define LOG_STRING_FULL(log, ...) 
+//#define LOG_STRING_LOG(log, ...) 
 
 void setup()
 {
+  String stringLogFull = LOG_STRING_FULL("salva na string o log, file, line e function");
+  String stringLogLog = LOG_STRING_LOG("recebe apenas o nome do log na string");
   uint8_t x = 10;
   String st = "ola";
-  LOG_BLE("TESTANDO NOVOS DEBUG %d - %s!", x, st.c_str());
+  LOG_LOG("TESTANDO NOVOS DEBUG %d - %s!", x, st.c_str());
   call_test();
 }
 
@@ -18,5 +25,5 @@ void loop()
 
 void call_test()
 {
-  LOG_BLE("TESTANDO NOVOS DEBUG EM OUTRAS FUNCOS!");
+  LOG_LOG("TESTANDO NOVOS DEBUG EM OUTRAS FUNCOES!");
 }
